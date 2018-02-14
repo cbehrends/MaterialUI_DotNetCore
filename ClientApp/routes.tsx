b82e26e4f5c59ffaa1448df970ffaf93d02a23d2
Corey Behrends <cbehrends@gmail.com>
2018-02-14 06:34:10 -0600

    Bit of cleanup

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
+
+
 import {default as Layout} from './components/Layout';
 import Home from './components/Home/Home';
 import Counter from './components/Counter/Counter';
 
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
+    <Route exact path='/' component={ Home }  />
     <Route exact path='/counter' component={ Counter } />
     
     
